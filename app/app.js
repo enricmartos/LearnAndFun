5d4c102c22684fc23054bd5486784e2999b6bffd
enricmartos <enric.martos@gmail.com>
2019-01-20 21:42:25 +0100

    Dockerize module with docker-compose

--- app/app.js
+++ app/app.js
@@ -115,5 +115,29 @@ app.use("/app", session_middleware);
 app.use("/app", routes_app);
 
 //Server execution
-server.listen(9001);
-mongoose.connect("mongodb://localhost/appdb");
+server.listen(3000);
+
+//DB connection
+
+const options = {
+    autoIndex: false, // Don't build indexes
+    reconnectTries: 30, // Retry up to 30 times
+    reconnectInterval: 500, // Reconnect every 500ms
+    poolSize: 10, // Maintain up to 10 socket connections
+    // If not connected, return errors immediately rather than waiting for reconnect
+    bufferMaxEntries: 0
+  }
+
+const connectWithRetry = () => {
+  console.log('MongoDB connection with retry')
+  mongoose.connect("mongodb://mongo:27017/appdb", options).then(()=>{
+    console.log('MongoDB is connected')
+  }).catch(err=>{
+    console.log('MongoDB connection unsuccessful, retry after 5 seconds.')
+    setTimeout(connectWithRetry, 5000)
+  })
+}
+
+connectWithRetry()
+
+//mongoose.connect("mongodb://mongo/appdb");
